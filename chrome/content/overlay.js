b9af6dc94a67e426ecd0a236f582b9f630ca0321
Jesse Andrews <jesse@mac-mini.local>
2008-12-13 16:26:55 -0800

    make it as large as possible up to 800px wide

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -5,7 +5,9 @@ var searchy = new function() {
 
   this.go = function() {
     var panel = $('searchy');
-    panel.openPopup($('urlbar'), 'after_start', -1, 50);
+    var width = Math.min(document.width - 40, 800);
+    panel.setAttribute('width', width);
+    panel.openPopup(document.getElementById('content'), 'overlap', (document.width-width)/2, 20);
     panel.focus();
   };
 
@@ -104,11 +106,11 @@ var searchy = new function() {
           var vbox = document.createElement('vbox');
           vbox.setAttribute('class', 'result');
           vbox.setAttribute('href', result.clickurl);
-          var title = document.createElementNS("http://www.w3.org/1999/xhtml","html:div");
+          var title = document.createElementNS("http://www.w3.org/1999/xhtml", "html:div");
           title.setAttribute('class', 'title');
           appendHTMLtoXUL(result.title, title);
           vbox.appendChild(title);
-          var description = document.createElementNS("http://www.w3.org/1999/xhtml","html:div");
+          var description = document.createElementNS("http://www.w3.org/1999/xhtml", "html:div");
           description.setAttribute('class', 'description');
           appendHTMLtoXUL(result.abstract, description);
           vbox.appendChild(description);
@@ -126,11 +128,23 @@ var searchy = new function() {
   function appendHTMLtoXUL(html, node) {
     html.split(/<b>(.*?<\/b>)|([^<]*)/).forEach(
       function(text) {
-        var span = document.createElementNS("http://www.w3.org/1999/xhtml","html:span");
+        var span = document.createElementNS("http://www.w3.org/1999/xhtml", "html:span");
         if (text.match(/<\/b>$/)) {
           span.setAttribute('class', 'bold');
           text = text.slice(0, text.length - 4);
         }
+
+        /* FIXME: instead of deleting <wbr>, instead create multiple spans
+         *        so the layout engine can wrap the layout - JA
+         */
+
+        text = text.replace(/<wbr>/g, "");
+
+        /* convert XML UTF-16 entities to string characters */
+        text = text.replace(/&#(\d+);/g, function() { return String.fromCharCode(RegExp.$1); });
+
+        /* convert some of the more popular XML entities in text */
+        text = text.replace(/&quote;/g, '"');
         text = text.replace(/&gt;/g, '>');
         text = text.replace(/&lt;/g, '<');
         text = text.replace(/&amp;/g, '&');

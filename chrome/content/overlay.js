29b9a39de56bbd62b13a075b09e097b32950c977
Jesse Andrews <anotherjesse@gmail.com>
2008-12-19 21:05:15 -0800

    less stuff

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -66,32 +66,7 @@ var searchy = new function() {
   }
 
   function appendHTMLtoXUL(html, node) {
-    html.split(/<b>(.*?<\/b>)|([^<]*)/).forEach(
-      function(text) {
-        var span = document.createElementNS("http://www.w3.org/1999/xhtml", "html:span");
-        if (text.match(/<\/b>$/)) {
-          span.setAttribute('class', 'bold');
-          text = text.slice(0, text.length - 4);
-        }
-
-        /* FIXME: instead of deleting <wbr>, instead create multiple spans
-         *        so the layout engine can wrap the layout - JA
-         */
-
-        text = text.replace(/<wbr>/g, "");
-
-        /* convert XML UTF-16 entities to string characters */
-        text = text.replace(/&#(\d+);/g, function() { return String.fromCharCode(RegExp.$1); });
-
-        /* convert some of the more popular XML entities in text */
-        text = text.replace(/&quot;/g, '"');
-        text = text.replace(/&gt;/g, '>');
-        text = text.replace(/&lt;/g, '<');
-        text = text.replace(/&amp;/g, '&');
-
-        span.appendChild(document.createTextNode(text));
-        node.appendChild(span);
-      });
+    node.appendChild(document.createTextNode(html));
   }
 
   setInterval(go, 100);

690233e4b7a2914faab95b268e123b0e1acd7a00
Jesse Andrews <anotherjesse@gmail.com>
2009-02-16 00:54:36 -0800

    initial hackage

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -230,7 +230,7 @@ var searchy = new function() {
 
         var json = nsJSON.decode(req.xhr.responseText);
 
-        if (!json.results) {
+        if (!json.entries) {
           return noresults();
         }
       }
@@ -241,38 +241,403 @@ var searchy = new function() {
       current = null;
       queried = req.input;
 
-      if (json.results.length == 0) {
+      if (json.entries.length == 0) {
         return noresults();
       }
 
       var box = $('searchy-results');
       // $('searchy-about-results').value = "Estimated Results: " + json.responseData.cursor.estimatedResultCount;
 
-      json.results.forEach(
+      json.entries.forEach(
         function(result) {
 /*
-          var sample = {
-            "text": "Writing maths- Test today... but looking forward to continuing my 3D- Project in Maya afterwards ;-)",
-            "to_user_id": null,
-            "from_user": "Omme",
-            "id": 1214632968,
-            "from_user_id": 3991046,
-            "iso_language_code": "en",
-            "profile_image_url": "http:\/\/s3.amazonaws.com\/twitter_production\/profile_images\/73361113\/mypictr_150x150_normal.jpg",
-            "created_at": "Mon, 16 Feb 2009 07:36:39 +0000"
-          };
+        {
+            "updated": "2009-02-16T01:47:25Z",
+            "service": {
+                "profileUrl": "http://friendfeed.com/dheydon",
+                "iconUrl": "http://friendfeed.com/static/images/icons/internal.png?v=e471e9afdf04ae568dcbddb5584fc6c0",
+                "id": "internal",
+                "entryType": "message",
+                "name": "FriendFeed"
+            },
+            "title": "What word would you use to describe people on friendfeed?",
+            "media": [
+
+            ],
+            "comments": [
+                {
+                    "date": "2009-02-16T01:48:15Z",
+                    "body": "other than friendly lol",
+                    "id": "f68a8bca-fb6d-40f4-9e6c-0e3662edd66a",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/dheydon",
+                        "nickname": "dheydon",
+                        "id": "64981b38-5d14-11dd-9bce-003048343a40",
+                        "name": "Darren Heydon"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:48:30Z",
+                    "body": "People?",
+                    "id": "7198b2a1-412b-41c8-b96d-6130bea6674d",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/schmeesy",
+                        "nickname": "schmeesy",
+                        "id": "7ab0207e-0db8-11dd-8c43-003048343a40",
+                        "name": "Lisa L. Seifert"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:50:01Z",
+                    "body": "Emotive",
+                    "id": "98199462-5dc7-4c01-9cb5-07f1a52943f9",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/dheydon",
+                        "nickname": "dheydon",
+                        "id": "64981b38-5d14-11dd-9bce-003048343a40",
+                        "name": "Darren Heydon"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:50:36Z",
+                    "body": "interesting.",
+                    "id": "f5bf844f-5b26-487a-9925-66478038f934",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/jvjannotti",
+                        "nickname": "jvjannotti",
+                        "id": "2f0cd52c-f396-11dc-8371-003048343a40",
+                        "name": "some have called me, Jim?"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:51:12Z",
+                    "body": "Lisa, i see what you did there :)",
+                    "id": "bb7c9828-f890-4dcc-bb98-b621ee59db4e",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/dheydon",
+                        "nickname": "dheydon",
+                        "id": "64981b38-5d14-11dd-9bce-003048343a40",
+                        "name": "Darren Heydon"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:51:43Z",
+                    "body": "human",
+                    "id": "a01cdca8-82ee-4973-947f-fb25e2187984",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/texasguitarbuster",
+                        "nickname": "texasguitarbuster",
+                        "id": "465df4f4-c938-11dd-8551-003048678d04",
+                        "name": "Greg (GuitarBuster)"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:52:22Z",
+                    "body": "Humorous!",
+                    "id": "6421133d-924e-4566-9a6c-5a942e723100",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/dheydon",
+                        "nickname": "dheydon",
+                        "id": "64981b38-5d14-11dd-9bce-003048343a40",
+                        "name": "Darren Heydon"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:53:13Z",
+                    "body": "Typers.",
+                    "id": "0027e6d0-59e7-4291-9815-be4f3689595a",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/faboomama",
+                        "nickname": "faboomama",
+                        "id": "a78335c4-3c7e-11dd-88bc-003048343a40",
+                        "name": "Anika Malone"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:53:46Z",
+                    "body": "Diverse",
+                    "id": "804851d7-3890-426c-a4b9-10d9186a5cb9",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/mvandenberg",
+                        "nickname": "mvandenberg",
+                        "id": "ee463e96-4545-11dd-a546-003048343a40",
+                        "name": "Mark VandenBerg"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:55:25Z",
+                    "body": "+1 MVB, that's what I was going to say.",
+                    "id": "4187356f-2e6b-4a47-912c-7cc022765881",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/jhuebel",
+                        "nickname": "jhuebel",
+                        "id": "3e15bcdc-454e-11dd-a546-003048343a40",
+                        "name": "Jason Huebel"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:57:03Z",
+                    "body": "*grin*",
+                    "id": "dd9a726f-58ad-450b-a5cc-22f8323c10d5",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/schmeesy",
+                        "nickname": "schmeesy",
+                        "id": "7ab0207e-0db8-11dd-8c43-003048343a40",
+                        "name": "Lisa L. Seifert"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:57:46Z",
+                    "body": "Slackers! (jk)",
+                    "id": "13971b8b-543f-458f-8261-1aa8692067df",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/rodfather",
+                        "nickname": "rodfather",
+                        "id": "5facb090-47b9-11dd-93ef-003048343a40",
+                        "name": "Rodfather"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:58:56Z",
+                    "body": "LOL",
+                    "id": "f004de75-3ea9-4c1c-a710-82e845d7ccca",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/dheydon",
+                        "nickname": "dheydon",
+                        "id": "64981b38-5d14-11dd-9bce-003048343a40",
+                        "name": "Darren Heydon"
+                    }
+                },
+                {
+                    "date": "2009-02-16T01:59:14Z",
+                    "body": "Awesomesauce!",
+                    "id": "8752eac6-66c0-4782-b5bd-659639262689",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/haggis",
+                        "nickname": "haggis",
+                        "id": "0d65b976-f520-11dc-87d3-003048343a40",
+                        "name": "Haggis (Sean)"
+                    }
+                },
+                {
+                    "date": "2009-02-16T02:04:42Z",
+                    "body": "Epic!",
+                    "id": "14c37fda-ed86-49f1-a74f-37b86eb22126",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/simplyx",
+                        "nickname": "simplyx",
+                        "id": "f367eecc-c283-11dd-8616-003048678d04",
+                        "name": "Christian (Simply X)"
+                    }
+                },
+                {
+                    "date": "2009-02-16T02:05:10Z",
+                    "body": "Hawt!",
+                    "id": "0ef353e0-60f0-4c7e-b970-c872d46708ff",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/dheydon",
+                        "nickname": "dheydon",
+                        "id": "64981b38-5d14-11dd-9bce-003048343a40",
+                        "name": "Darren Heydon"
+                    }
+                },
+                {
+                    "date": "2009-02-16T02:06:29Z",
+                    "body": "filterable!",
+                    "id": "a8118a58-9d63-4593-9201-c5600b96a3e6",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/gamjee",
+                        "nickname": "gamjee",
+                        "id": "7b306422-1b29-11dd-b75f-003048343a40",
+                        "name": "Geoff Schultz"
+                    }
+                },
+                {
+                    "date": "2009-02-16T02:07:52Z",
+                    "body": "+1 for Darren!!",
+                    "id": "a757dcc3-08f8-4cce-894c-72eb553020c0",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/micahbear78",
+                        "nickname": "micahbear78",
+                        "id": "b59e4454-ee00-4e3d-a777-8f5d5e626018",
+                        "name": "Micah"
+                    }
+                },
+                {
+                    "date": "2009-02-16T02:14:53Z",
+                    "body": "*blush*",
+                    "id": "dd3870c0-1948-42e7-a5d9-2db594217f32",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/dheydon",
+                        "nickname": "dheydon",
+                        "id": "64981b38-5d14-11dd-9bce-003048343a40",
+                        "name": "Darren Heydon"
+                    }
+                },
+                {
+                    "date": "2009-02-16T02:24:31Z",
+                    "body": "pushers",
+                    "id": "e6329c71-82e0-4670-86bd-8755a891d9c0",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/minimage",
+                        "nickname": "minimage",
+                        "id": "92101c20-f17e-11dc-963f-003048343a40",
+                        "name": "MiniMageAdoptsBorgBabies"
+                    }
+                },
+                {
+                    "date": "2009-02-16T04:10:42Z",
+                    "body": "Verbose.",
+                    "id": "471aa979-e604-4224-9e0a-21639a1925b7",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/silas216",
+                        "nickname": "silas216",
+                        "id": "5afdbc6e-2a05-11dd-aed7-003048343a40",
+                        "name": "Steven Perez"
+                    }
+                },
+                {
+                    "date": "2009-02-16T04:13:43Z",
+                    "body": "Was gonna say Diverse, but Mark took it. I say Skilled.",
+                    "id": "429782ab-66cb-4ba9-959b-730b8d683632",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/genieyclo",
+                        "nickname": "genieyclo",
+                        "id": "00268190-5ef8-11dd-85cc-003048343a40",
+                        "name": "Mohomed.Genieyclo.Gatsby"
+                    }
+                },
+                {
+                    "date": "2009-02-16T04:15:06Z",
+                    "body": "Bright.",
+                    "id": "cea33a68-4eee-491c-ae23-2464fe390295",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/spragued",
+                        "nickname": "spragued",
+                        "id": "722a04ba-19d9-11dd-b253-003048343a40",
+                        "name": "Sprague D"
+                    }
+                },
+                {
+                    "date": "2009-02-16T04:15:22Z",
+                    "body": "Diverse",
+                    "id": "37baa4ca-ca1e-4fce-a9b1-db4ee186b450",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/duckscratch",
+                        "nickname": "duckscratch",
+                        "id": "0ae42308-f57b-11dc-80a0-003048343a40",
+                        "name": "Bryce"
+                    }
+                },
+                {
+                    "date": "2009-02-16T04:16:18Z",
+                    "body": "Supercalifragilisticexpialidocious.",
+                    "id": "e8b21b44-08b8-4caf-9935-0c3e52d507b5",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/boycaught",
+                        "nickname": "boycaught",
+                        "id": "fc06c882-9df7-4ed3-9da2-54ac2b64d795",
+                        "name": ".LAG"
+                    }
+                },
+                {
+                    "date": "2009-02-16T04:18:36Z",
+                    "body": "pleasant",
+                    "id": "04a7e01e-abce-4797-8bfe-bfcf0d8bf8ba",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/eyebee",
+                        "nickname": "eyebee",
+                        "id": "fd410d92-4944-11dd-9c86-003048343a40",
+                        "name": "Ian May"
+                    }
+                },
+                {
+                    "date": "2009-02-16T04:19:57Z",
+                    "body": "Visual",
+                    "id": "f40660df-05d6-4030-ba38-0b5781d60f7e",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/dheydon",
+                        "nickname": "dheydon",
+                        "id": "64981b38-5d14-11dd-9bce-003048343a40",
+                        "name": "Darren Heydon"
+                    }
+                },
+                {
+                    "date": "2009-02-16T05:45:59Z",
+                    "body": "apopemptoclinic",
+                    "id": "960ae144-63f2-4bf4-80ae-ed4cc99ddbe6",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/mrwondrous",
+                        "nickname": "mrwondrous",
+                        "id": "fc23f716-f75b-11dc-b955-003048343a40",
+                        "name": "david beckwith"
+                    }
+                },
+                {
+                    "date": "2009-02-16T08:06:05Z",
+                    "body": "Insomniacs",
+                    "id": "4d88c47c-47e7-4c85-958b-8c31339e42c7",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/charlieanzman",
+                        "nickname": "charlieanzman",
+                        "id": "a84135a3-0e7f-4197-aad4-14c3f82693de",
+                        "name": "Charlie Anzman"
+                    }
+                },
+                {
+                    "date": "2009-02-16T08:09:35Z",
+                    "body": "Stabbity",
+                    "id": "4c0673bb-4c17-4ee5-b86a-16b0ca04dcce",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/itblogger",
+                        "nickname": "itblogger",
+                        "id": "fb86e966-5c60-11dd-8697-003048343a40",
+                        "name": "Alex Scoble"
+                    }
+                },
+                {
+                    "date": "2009-02-16T08:09:49Z",
+                    "body": "friends",
+                    "id": "c68c0647-8415-4585-b20b-f979536b3546",
+                    "user": {
+                        "profileUrl": "http://friendfeed.com/dffrnt",
+                        "nickname": "dffrnt",
+                        "id": "f7e5b35c-0dbc-11dd-a773-003048343a40",
+                        "name": "vijay"
+                    }
+                }
+            ],
+            "link": "http://friendfeed.com/e/1a5554ff-c117-4d63-ba0a-ae41adacde3b",
+            "likes": [
+            ],
+            "anonymous": false,
+            "published": "2009-02-16T01:47:25Z",
+            "hidden": false,
+            "id": "1a5554ff-c117-4d63-ba0a-ae41adacde3b",
+            "user": {
+                "profileUrl": "http://friendfeed.com/dheydon",
+                "nickname": "dheydon",
+                "id": "64981b38-5d14-11dd-9bce-003048343a40",
+                "name": "Darren Heydon"
+            }
+        },
+
 */
 
           var vbox = document.createElement('hbox');
-          vbox.setAttribute('class', 'result twitter');
-          vbox.setAttribute('href', 'http://twitter.com/' + result.from_user + '/status/' + result.id);
-          var avatar = document.createElementNS("http://www.w3.org/1999/xhtml", "html:img");
-          avatar.setAttribute('src', result.profile_image_url);
-          vbox.appendChild(avatar);
+          vbox.setAttribute('class', 'result friendfeed');
+          vbox.setAttribute('href', result.link);
+          var container = document.createElement('vbox');
+          container.setAttribute('pack', 'top');
+          var service = document.createElementNS("http://www.w3.org/1999/xhtml", "html:img");
+          service.setAttribute('src', result.service.iconUrl);
+          service.setAttribute('tooltiptext', result.service.name);
+          container.appendChild(service);
+          vbox.appendChild(container);
           var title = document.createElementNS("http://www.w3.org/1999/xhtml", "html:div");
           title.setAttribute('class', 'title');
           title.setAttribute('flex', 1);
-          appendHTMLtoXUL(result.text, title);
+          appendHTMLtoXUL(result.title, title);
           vbox.appendChild(title);
           box.appendChild(vbox);
 
@@ -298,7 +663,7 @@ var searchy = new function() {
     }
 
     function urlFor(search) {
-      var base = "http://search.twitter.com/search.json?q=%QUERY%";
+      var base = "http://friendfeed.com/api/feed/search?q=%QUERY%";
 
 /*      if ((search[0] == '@') && currentHost()) {
         search = search.slice(1) + " site:" + currentHost();
